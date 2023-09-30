f0cf96adbd8d4044c1eb2a99fa584d489fe4bdda
Carlos Azaustre <cazaustre@gmail.com>
2023-09-30 23:01:22 +0200

    add custom hook to listen keyboard events

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,8 +2,11 @@ import { useRef, useEffect } from "react";
 import { useFrame, useThree } from "@react-three/fiber";
 import { useSphere } from "@react-three/cannon";
 import { Vector3 } from "three";
+import { useKeyboard } from "../hooks/useKeyboard";
 
 export const Player = () => {
+	const actions = useKeyboard();
+
 	const { camera } = useThree();
 	const [ref, api] = useSphere(() => ({
 		mass: 1,
