fc547edd8924eb40ade50376731614bcf48bd7db
Carlos Azaustre <cazaustre@gmail.com>
2023-10-01 21:16:10 +0200

    add x,y movements and jump

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,8 +4,12 @@ import { useSphere } from "@react-three/cannon";
 import { Vector3 } from "three";
 import { useKeyboard } from "../hooks/useKeyboard";
 
+const JUMP_FORCE = 4;
+const SPEED = 4;
+
 export const Player = () => {
-	const actions = useKeyboard();
+	const { moveBackward, moveForward, moveLeft, moveRight, jump } =
+		useKeyboard();
 
 	const { camera } = useThree();
 	const [ref, api] = useSphere(() => ({
@@ -32,6 +36,29 @@ export const Player = () => {
 		camera.position.copy(
 			new Vector3(pos.current[0], pos.current[1], pos.current[2])
 		);
+
+		const direction = new Vector3();
+		const frontVector = new Vector3(
+			0,
+			0,
+			(moveBackward ? 1 : 0) - (moveForward ? 1 : 0)
+		);
+		const sideVector = new Vector3(
+			(moveLeft ? 1 : 0) - (moveRight ? 1 : 0),
+			0,
+			0
+		);
+		direction
+			.subVectors(frontVector, sideVector)
+			.normalize()
+			.multiplyScalar(SPEED)
+			.applyEuler(camera.rotation);
+
+		api.velocity.set(direction.x, vel.current[1], direction.z);
+
+		if (jump && Math.abs(vel.current[1]) < 0.05) {
+			api.velocity.set(vel.current[0], JUMP_FORCE, vel.current[2]);
+		}
 	});
 
 	return <mesh ref={ref}></mesh>;
