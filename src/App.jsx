8c22c52a74cab55b7d883adf832b761a24837062
Carlos Azaustre <cazaustre@gmail.com>
2023-10-02 19:58:41 +0200

    add player camera

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import { Sky } from "@react-three/drei";
 import { Physics } from "@react-three/cannon";
 import { Ground } from "./components/Ground";
 import { Player } from "./components/Player";
+import { FPV } from "./components/FPV";
 
 function App() {
 	return (
 		<>
 			<Canvas>
 				<Sky sunPosition={[100, 100, 20]} />
-				<ambientLight intensity={0.5} />
+				<ambientLight intensity={1.1} />
+				<FPV />
 				<Physics>
 					<Player />
 					<Ground />
 				</Physics>
 			</Canvas>
+			<div className="absolute centered cursor">+</div>
 		</>
 	);
 }
