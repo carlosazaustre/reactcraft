8e4962c30caa9c7ab09f9a24e5808f33f67aad83
Carlos Azaustre <cazaustre@gmail.com>
2023-11-18 12:36:03 +0100

    add xr controllers

--- src/components/Cubes.jsx
+++ src/components/Cubes.jsx
@@ -3,7 +3,6 @@ import { Cube } from "./Cube";
 
 export const Cubes = () => {
 	const [cubes] = useStore((state) => [state.cubes]);
-	console.log(cubes);
 
 	return cubes.map(({ key, pos, texture }) => {
 		return <Cube key={key} position={pos} texture={texture} />;
